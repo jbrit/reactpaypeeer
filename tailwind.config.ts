3ab27816e2c3e2b079f8c0815db7b3366eeaeeed
Akindeji <akindejioluwaseun015@gmail.com>
2024-02-05 03:05:53 +0100

    chore : add custom config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,11 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "grid-lines": "url(../public/Grid Lines.svg)",
+      },
+      colors: {
+        black1: "#1F1F1F",
+        gray1: "#3C3C3C",
       },
     },
   },
