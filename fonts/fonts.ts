559a985be045090c8445b1380422936410cef8e4
Akindeji <akindejioluwaseun015@gmail.com>
2024-02-05 03:07:02 +0100

    chore : setup local fonts

---
+++ fonts/fonts.ts
@@ -0,0 +1,36 @@
+import localFont from "next/font/local";
+
+export const axiforma = localFont({
+  src: [
+    {
+      path: "./Axiforma-Regular.woff",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./Axiforma-SemiBold.woff",
+      weight: "600",
+      style: "normal",
+    },
+  ],
+});
+
+export const GTWalsheim = localFont({
+  src: [
+    {
+      path: "./GTWalsheimPro-Regular.woff",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./GTWalsheimPro-Medium.woff",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./GTWalsheimPro-Bold.woff",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+});
